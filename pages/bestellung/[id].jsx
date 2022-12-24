e3d7834a6dc7e22774cce74599f644775c1017c6
JoernSchoenyan <joern.schoenyan@web.de>
2022-12-24 15:56:52 +0100

    Bestellungsübersicht

---
+++ pages/bestellung/[id].jsx
@@ -0,0 +1,101 @@
+import styles from '../../styles/Order.module.css'
+import React from 'react'
+import Image from 'next/image'
+import CartTotal from '../../components/CartTotal'
+import { File, CheckSquare, Map, Truck, Coffee } from 'react-feather'
+
+const Order = () => {
+  const status = 0;
+    
+  const statusClass = (index) => {
+    if (index - status < 1 ) return styles.done
+    if (index - status === 1 ) return styles.inProgress
+    if (index - status > 1 ) return styles.notDone
+  }
+
+  return (
+    <div className={styles.container}>
+        <div className={styles.left}>
+          <span>Bestellung #523624572546</span>
+          <div className={styles.row}>
+
+            <div className={statusClass(0)}>
+              <File size={36} strokeWidth={2.5} />
+              <span>Bestellung</span>
+              <div className={styles.checkedIcon}>
+                <CheckSquare size={36} strokeWidth={2.5} color='green' />
+              </div>
+            </div>
+            <div className={statusClass(1)}>
+              <Coffee size={36} strokeWidth={2.5} />
+              <span>Zubereitung</span>
+              <div className={styles.checkedIcon}>
+                <CheckSquare size={36} strokeWidth={2.5} color='green' />
+              </div>
+            </div>
+            <div className={statusClass(2)}>
+              <Map size={36} strokeWidth={2.5} />
+              <span>Unterwegs</span>
+              <div className={styles.checkedIcon}>
+                <CheckSquare size={36} strokeWidth={2.5} color='green' />
+              </div>
+            </div>
+            <div className={statusClass(3)}>
+              <Truck size={36} strokeWidth={2.5} />
+              <span>Auslieferung</span>
+              <div className={styles.checkedIcon}>
+                <CheckSquare size={36} strokeWidth={2.5} color='green' />
+              </div>
+            </div>
+          </div>
+          <span>Die Bestellung enthält:</span>
+          <table className={styles.table}>
+              <thead>
+                  <tr className={styles.tr}>
+                      <th>Produkt</th>
+                      <th>Name</th>
+                      <th>Extras</th>
+                      <th>Preis</th>
+                      <th>Anzahl</th>
+                      <th style={{textAlign: "right"}}>Summe</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <div className={styles.imageContainer}>
+                        <Image src="/img/logo.svg" alt="" fill objectFit="cover" />
+                      </div>
+                    </td>
+                    <td>
+                      <span className={styles.name}>Dönerpizza</span>
+                    </td>
+                    <td>
+                      <span className={styles.extras}>Scharf</span>
+                    </td>
+                    <td style={{textAlign: "center"}}>
+                      <span className={styles.price}>15 €</span>
+                    </td>
+                    <td style={{textAlign: "center"}}>
+                      <span className={styles.quantity}>2</span>
+                    </td>
+                    <td style={{textAlign: "right"}}>
+                      <span className={styles.total}>30 €</span>
+                    </td>
+                  </tr>
+                </tbody>
+            </table>
+        </div>
+        <div className={styles.right}>
+            <div className={styles.orderDetails}>
+              <span>Helga Hoppenstedt</span>
+              <span>Musterstraße 123a</span>
+              <span>38350 Helmstedt</span>
+            </div>
+            <CartTotal />
+        </div>
+    </div>
+  )
+}
+
+export default Order
