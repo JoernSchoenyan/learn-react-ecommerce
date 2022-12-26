22f2ff36b6716c9a1fbe68a143a8a2565296fbe5
JoernSchoenyan <joern.schoenyan@web.de>
2022-12-26 18:32:42 +0100

    Bereich für Datenschmutz etc im Footer eingeführt

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,8 @@
 import styles from '../styles/Footer.module.css'
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
+
 
 const Footer = () => {
   return (
@@ -10,15 +12,26 @@ const Footer = () => {
       </div>
       <div className={styles.item}>
         <div className={styles.card}>
-          <h3>PLACEHOLDER</h3>
+          <div><h3>PLACEHOLDER</h3></div>
         </div>
         <div className={styles.card}>
-          <h3 className={styles.title}>UNSER RESTAURANT</h3>
-          <p className={styles.text}>Musterstraße 123<br />38350 Helmstedt</p>
+          <div>
+            <h3 className={styles.title}>UNSER RESTAURANT</h3>
+            <p className={styles.text}>Musterstraße 123<br />38350 Helmstedt</p>
+          </div>
         </div>
         <div className={styles.card}>
-          <h3 className={styles.title}>ÖFFNUNGSZEITEN</h3>
-          <p className={styles.text}>Nicht geöffnet.<br />Dies ist nur eine Demo.<br />Bestellungen werden nicht ausgeführt!</p>
+          <div>
+            <h3 className={styles.title}>ÖFFNUNGSZEITEN</h3>
+            <p className={styles.text}>Nicht geöffnet.<br />Dies ist nur eine Demo.<br />Bestellungen werden nicht ausgeführt!</p>
+          </div>
+          
+          <div className={styles.legalNotice}>
+            <ul className={styles.legalLinks}>
+              <li><Link href="/impressum">Impressum</Link></li>
+              <li><Link href="/datenschutz">Datenschutz</Link></li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
