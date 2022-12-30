9ebddc6749dece3bc3abe5f63f3425989635e602
JoernSchoenyan <joern.schoenyan@web.de>
2022-12-30 12:28:42 +0100

    CartTotal.jsx nutzt jetzt Redux
    Memo an mich selbst: was ich da gestern fabriziert habe, war ja mindestens mittlerer Humbug. Warum prop drilling, wenn ich redux zur Verfügung habe? Ich muss da nur aufpassen, wenn ich die Komponente in einer abgeschickten Bestellung wiederverwende, dass ich dort die passenden Daten so nicht mehr zur Verfügung habe (falls ich das alles richtig verstehe).

--- components/CartTotal.jsx
+++ components/CartTotal.jsx
@@ -1,13 +1,15 @@
 import styles from '../styles/CartTotal.module.css'
 import React from 'react'
-import cartSlice from '../redux/cartSlice'
+import { useSelector } from 'react-redux'
 
-const CartTotal = ({checkoutButton, total}) => {
+const CartTotal = ({checkoutButton}) => {
+  const cart = useSelector((state) => state.cart);
+
   return (
     <div className={styles.container}>
                 <h2 className={styles.title}>GESAMTPREIS</h2>
                 <div className={styles.totalText}>
-                  <b className={styles.totalTextTitle}>Zwischensumme:</b>{total} €
+                  <b className={styles.totalTextTitle}>Zwischensumme:</b>{cart.total} €
                 </div>
                 <div className={styles.totalText}>
                   <b className={styles.totalTextTitle}>Rabatt:</b>0 €
@@ -16,7 +18,7 @@ const CartTotal = ({checkoutButton, total}) => {
                   <b className={styles.totalTextTitle}>Lieferkosten:</b>0 €
                 </div>
                 <div className={styles.totalText}>
-                  <b className={styles.totalTextTitle}>Summe:</b>{total} €
+                  <b className={styles.totalTextTitle}>Summe:</b>{cart.total} €
                 </div>
                 {checkoutButton 
                   ? <button className={styles.button}>KOSTENPFLICHTIG BESTELLEN</button>
