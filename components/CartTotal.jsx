998d1651935b3ac274c771279185466e83e39000
JoernSchoenyan <joern.schoenyan@web.de>
2023-01-01 17:57:24 +0100

    Anfang PayPal-Integration
    CartTotal werde ich wieder entfernen

--- components/CartTotal.jsx
+++ components/CartTotal.jsx
@@ -6,24 +6,7 @@ const CartTotal = ({checkoutButton}) => {
   const cart = useSelector((state) => state.cart);
 
   return (
-    <div className={styles.container}>
-                <h2 className={styles.title}>GESAMTPREIS</h2>
-                <div className={styles.totalText}>
-                  <b className={styles.totalTextTitle}>Zwischensumme:</b>{cart.total} €
-                </div>
-                <div className={styles.totalText}>
-                  <b className={styles.totalTextTitle}>Rabatt:</b>0 €
-                </div>
-                <div className={styles.totalText}>
-                  <b className={styles.totalTextTitle}>Lieferkosten:</b>0 €
-                </div>
-                <div className={styles.totalText}>
-                  <b className={styles.totalTextTitle}>Summe:</b>{cart.total} €
-                </div>
-                {checkoutButton 
-                  ? <button className={styles.button}>KOSTENPFLICHTIG BESTELLEN</button>
-                  : <button className={styles.button} disabled>BEZAHLT</button>}
-            </div>
+    <></>
   )
 }
 
--- pages/warenkorb.jsx
+++ pages/warenkorb.jsx
@@ -3,10 +3,73 @@ import React from 'react'
 import Image from 'next/image'
 import CartTotal from '../components/CartTotal'
 import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useState } from "react";
+import {
+    PayPalScriptProvider,
+    PayPalButtons,
+    usePayPalScriptReducer
+} from "@paypal/react-paypal-js";
 
 const Cart = () => {
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
+  const [showPayPal, setShowPayPal] = useState(true);
+
+  const amount = "2";
+  const currency = "EUR";
+  const style = {"layout": "vertical"};
+
+  // https://paypal.github.io/react-paypal-js/?path=/docs/example-paypalbuttons--default
+  // Custom component to wrap the PayPalButtons and handle currency changes
+  const ButtonWrapper = ({ currency, showSpinner }) => {
+  // usePayPalScriptReducer can be use only inside children of PayPalScriptProviders
+  // This is the main reason to wrap the PayPalButtons in a new component
+    const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
+
+    useEffect(() => {
+        dispatch({
+            type: "resetOptions",
+            value: {
+                ...options,
+                currency: currency,
+            },
+        });
+    }, [currency, showSpinner]);
+
+
+    return (<>
+            { (showSpinner && isPending) && <div className="spinner" /> }
+            <PayPalButtons
+                style={style}
+                disabled={false}
+                forceReRender={[amount, currency, style]}
+                fundingSource={undefined}
+                createOrder={(data, actions) => {
+                    return actions.order
+                        .create({
+                            purchase_units: [
+                                {
+                                    amount: {
+                                        currency_code: currency,
+                                        value: amount,
+                                    },
+                                },
+                            ],
+                        })
+                        .then((orderId) => {
+                            // Your code here after create the order
+                            return orderId;
+                        });
+                }}
+                onApprove={function (data, actions) {
+                    return actions.order.capture().then(function () {
+                        // Your code here after capture the order
+                    });
+                }}
+            />
+        </>
+    );
+  }
 
   return (
     <div className={styles.container}>
@@ -58,7 +121,36 @@ const Cart = () => {
         </div>
 
         <div className={styles.right}>
-            <CartTotal checkoutButton total={cart.total} />
+            <div className={styles.summary}>
+                <h2 className={styles.title}>GESAMTPREIS</h2>
+                <div className={styles.totalText}>
+                  <b className={styles.totalTextTitle}>Zwischensumme:</b>{cart.total} €
+                </div>
+                <div className={styles.totalText}>
+                  <b className={styles.totalTextTitle}>Rabatt:</b>0 €
+                </div>
+                <div className={styles.totalText}>
+                  <b className={styles.totalTextTitle}>Lieferkosten:</b>0 €
+                </div>
+                <div className={styles.totalText}>
+                  <b className={styles.totalTextTitle}>Summe:</b>{cart.total} €
+                </div>
+                <button className={styles.button}>KOSTENPFLICHTIG BESTELLEN UND BAR BEZAHLEN</button>
+                {showPayPal ? (<PayPalScriptProvider
+                    options={{
+                        "client-id": "test",
+                        components: "buttons",
+                        currency: "EUR"
+                    }}
+                >
+                <ButtonWrapper
+                        currency={currency}
+                        showSpinner={false}
+                    />
+                </PayPalScriptProvider>) : (<></>)}
+            </div>
+            
+            
         </div>
     </div>
   )
